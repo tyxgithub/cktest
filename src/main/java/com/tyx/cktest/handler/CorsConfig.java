94411273d8238af9232f6838b2bc074710bc42db
tyxgit <1241859914@qq.com>
2020-12-12 23:10:55 +0800

    20201212-3

--- src/main/java/com/tyx/cktest/handler/CorsConfig.java
+++ src/main/java/com/tyx/cktest/handler/CorsConfig.java
@@ -1,33 +1,34 @@
-//package com.tyx.cktest.handler;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//import org.springframework.web.filter.CorsFilter;
-//
-//@Configuration
-//public class CorsConfig {
-//
-//	private CorsConfiguration buildConfig() {
-//		CorsConfiguration corsConfiguration = new CorsConfiguration();
-//		corsConfiguration.setAllowCredentials(true);
-//		corsConfiguration.addAllowedOrigin("*"); // 1
-//
-//		corsConfiguration.addAllowedHeader("*"); // 2
-//
-//		corsConfiguration.addAllowedMethod("*"); // 3
-//		return corsConfiguration;
-//	}
-//
-//	@Bean
-//	public CorsFilter corsFilter() {
-//
-//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//
-//		source.registerCorsConfiguration("/**", buildConfig()); // 4
-//
-//		return new CorsFilter(source);
-//	}
-//
-//}
+package com.tyx.cktest.handler;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author ***
+ * @ClassName CorsConfig
+ * @Description 跨域处理
+ * @create ***
+ * @desc ***
+ **/
+
+@Configuration
+public class CorsConfig {
+	private CorsConfiguration buildConfig(){
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.addAllowedOrigin("*"); //允许任何域名使用
+		corsConfiguration.addAllowedHeader("*"); //允许任何头
+		corsConfiguration.addAllowedMethod("*"); //允许任何请求方法（post、get等）
+		return  corsConfiguration;
+	}
+
+	@Bean
+	public CorsFilter corsFilter(){
+
+		UrlBasedCorsConfigurationSource source = new  UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**",buildConfig());
+		return new CorsFilter(source);
+	}
+}
